2ba8b8fcd53399aacc6604755f2879a256dec8ad
JÆSURo <amantaiulyzhandos2005@gmail.com>
2025-10-14 21:20:49 +0500

    Clean commit

--- superset_config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
-SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///superset.db')
